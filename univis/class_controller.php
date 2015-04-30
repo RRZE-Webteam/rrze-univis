7e0a152688bc4a480e6d2117c31207a52591b252
Stefan Lindner <you@example.com>
2015-04-30 18:27:59 +0200

    small changes in templates

--- univis/class_controller.php
+++ univis/class_controller.php
@@ -59,7 +59,7 @@ class univisController {
 			//$daten["assets"] = $assets->holeDaten();
 
 			// Daten rendern
-			/*print("<pre>");
+		/*	print("<pre>");
 				print_r($daten);
 				print("</pre>");
 */
