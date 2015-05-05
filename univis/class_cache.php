ac61f3a05bcb158cdbfb3d84b7d81893a06b9d3e
Stefan Lindner <stefan.lindner@fau.de>
2015-05-05 12:52:10 +0200

    cache working

--- univis/class_cache.php
+++ univis/class_cache.php
@@ -25,8 +25,9 @@ class univisCache {
 	 */
 	function __construct($optionen) {
             //self::$path = plugins_url( "cache_dir", __FILE__);
-            self::$path = dirname( plugin_basename( __FILE__ ) )."/cache_dir";
-		$this->optionen = $optionen;
+           // self::$path = dirname( plugin_basename( __FILE__ ) )."/cache_dir";
+					self::$path = plugin_dir_path( __FILE__ )."cache_dir";
+				$this->optionen = $optionen;
 		
 	}
 
@@ -36,25 +37,25 @@ class univisCache {
 		
 		if($sucheDateiErgebnis > 0) {
 			// Datei ist vorhanden und gueltig
+			echo "<!-- Univis Datensatz vom ".date("d.m.y H:i:s",filemtime($this->filepath()))." -->\n";
 			return file_get_contents($this->filepath());
 		}
 
 		if($sucheDateiErgebnis == 0 && $force) {
 			// Datei nicht gueltig, aber:
 			// Ausgabe durch Parameter forciert.
+			echo "<!-- Univis Datensatz vom ".date("d.m.y H:i:s",filemtime($this->filepath()))." (force=true) -->\n";
 			return file_get_contents($this->filepath());
-		
 		}
-
+			echo "<!-- Univis Datensatz aktuell -->";
 		return -1;
 	}
 
 	public function setzeDaten($data) {
-		
-		$filepath = $this->filepath();
-                if(file_exists($filepath)) {
-                    file_put_contents($filepath, $data);
-                }
+				$filepath = $this->filepath();
+        if(is_dir(self::$path)) {
+							file_put_contents($filepath, $data);
+       }
 	}
 
 	private function sucheDatei() {
