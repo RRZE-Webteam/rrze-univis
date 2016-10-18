30b5b36723c1d09a5fe3ffb94472057b65e67859
endv02 <karin.kimpan@fau.de>
2016-10-18 16:08:19 +0200

    Korrektur Reihenfolge lecturetypen

--- univis/univis_dicts.php
+++ univis/univis_dicts.php
@@ -1,136 +1,134 @@
 <?php
-	
-	class univisDicts {
-		public static $acronyms = array(
-		    "Dr."   =>  "Doktor",
-		    "Prof." =>  "Professor",
-		    "Dipl." =>  "Diplom",
-		    "Inf."  =>  "Informatik",
-		    "Wi."   =>  "Wirtschaftsinformatik",
-		    "Ma."   =>  "Mathematik",
-		    "Ing."  =>  "Ingenieurwissenschaft",
-		    "B.A."  =>  "Bakkalaureus",
-		    "M.A."  =>  "Magister Artium",
-		    "phil." =>  "Geisteswissenschaft",
-		    "pol." =>  "Politikwissenschaft",
-		    "nat." =>  "Naturwissenschaft",
-		    "soc."  =>  "Sozialwissenschaft",
-		    "techn."    =>  "technische Wissenschaften",
-		    "vet.med." =>  "Tiermedizin",
-		    "med.dent."    =>  "Zahnmedizin",
-		    "h.c."  =>  "ehrenhalber",
-		    "med."  =>  "Medizin",
-		    "jur."  =>  "Recht",
-		    "rer."  =>  ""
-		);
 
-		public static $lecturetypen = array(
-		    "awa" => "Anleitung zu wiss. Arbeiten  (AWA)",
-			"ag" => "Arbeitsgemeinschaft  (AG)",
-			"ak" => "Aufbaukurs  (AK)",
-                        "as" => "Aufbauseminar  (AS)",
-			"ek" => "Einführungskurs  (EK)",
-                        "es" => "Examensseminar  (ES)",
-			"ex" => "Exkursion  (EX)",
-			"gk" => "Grundkurs  (GK)",
-                        "gs" => "Grundseminar  (GS)",
-			"ts" => "Theorieseminar  (TS)",
-			"sem" => "Seminar  (SEM)",
-			"hs" => "Hauptseminar  (HS)",
-			"kk" => "Klausurenkurs  (KK)",
-			"klv" => "Klinische Visite  (KLV)",
-			"ko" => "Kolloquium  (KO)",
-                        "ks" => "Kombiseminar  (KS)",
-			"ku" => "Kurs  (KU)",
-                        "mas" => "Masterseminar  (MAS)",
-			"ms" => "Mittelseminar  (MS)",
-			"os" => "Oberseminar  (OS)",
-			"pr" => "Praktikum  (PR)",
-			"prs" => "Praxisseminar  (PRS)",
-			"pjs" => "Projektseminar  (PJS)",
-			"ps" => "Proseminar  (PS)",
-			"re" => "Repetitorium  (RE)",
-			"sl" => "Sonstige Lehrveranstaltung  (SL)",
-			"tut" => "Tutorium  (TUT)",
-			"ue" => "Übung  (UE)",
-			"vorl" => "Vorlesung  (VORL)",
-			"v-ue" => "Vorlesung mit Übung  (V/UE)",
-			"hvl" => "Hauptvorlesung  (HVL)",
-			"pf" => "Prüfung  (PF)",
-			"gsz" => "Gremiensitzung  (GSZ)"
-		);
+class univisDicts {
 
-		public static $lecturetypen_short = array(
-		    "awa" => "Anleitung zu wiss. Arbeiten",
-			"ag" => "Arbeitsgemeinschaft",
-			"ak" => "Aufbaukurs",
-                        "as" => "Aufbauseminar",
-			"ek" => "Einführungskurs",
-                        "es" => "Examensseminar",
-			"ex" => "Exkursion",
-			"gk" => "Grundkurs",
-                        "gs" => "Grundseminar",
-			"ts" => "Theorieseminar",
-			"sem" => "Seminar",
-			"hs" => "Hauptseminar",
-			"kk" => "Klausurenkurs",
-			"klv" => "Klinische Visite",
-			"ko" => "Kolloquium",
-                        "ks" => "Kombiseminar",
-			"ku" => "Kurs",
-                        "mas" => "Masterseminar",
-			"ms" => "Mittelseminar",
-			"os" => "Oberseminar",
-			"pr" => "Praktikum",
-			"prs" => "Praxisseminar",
-			"pjs" => "Projektseminar",
-			"ps" => "Proseminar",
-			"re" => "Repetitorium",
-			"sl" => "Sonstige Lehrveranstaltung",
-			"tut" => "Tutorium",
-			"ue" => "Übung",
-			"vorl" => "Vorlesung",
-			"v-ue" => "Vorlesung mit Übung",
-			"hvl" => "Hauptvorlesung",
-			"pf" => "Prüfung",
-			"gsz" => "Gremiensitzung"
-		);
+    public static $acronyms = array(
+        "Dr." => "Doktor",
+        "Prof." => "Professor",
+        "Dipl." => "Diplom",
+        "Inf." => "Informatik",
+        "Wi." => "Wirtschaftsinformatik",
+        "Ma." => "Mathematik",
+        "Ing." => "Ingenieurwissenschaft",
+        "B.A." => "Bakkalaureus",
+        "M.A." => "Magister Artium",
+        "phil." => "Geisteswissenschaft",
+        "pol." => "Politikwissenschaft",
+        "nat." => "Naturwissenschaft",
+        "soc." => "Sozialwissenschaft",
+        "techn." => "technische Wissenschaften",
+        "vet.med." => "Tiermedizin",
+        "med.dent." => "Zahnmedizin",
+        "h.c." => "ehrenhalber",
+        "med." => "Medizin",
+        "jur." => "Recht",
+        "rer." => ""
+    );
+    
+    // Reihenfolge wichtig, da sonst Mehrfachersetzung von z.B. sem
+    public static $lecturetypen = array(
+        "awa" => "Anleitung zu wiss. Arbeiten  (AWA)",
+        "ag" => "Arbeitsgemeinschaft  (AG)",
+        "ak" => "Aufbaukurs  (AK)",
+        "ek" => "Einführungskurs  (EK)",
+        "ex" => "Exkursion  (EX)",
+        "gk" => "Grundkurs  (GK)",
+        "sem" => "Seminar  (SEM)",
+        "ts" => "Theorieseminar  (TS)",
+        "es" => "Examensseminar  (ES)",
+        "mas" => "MA-Seminar  (MAS)", 
+        "as" => "Aufbauseminar  (AS)",
+        "gs" => "Grundseminar  (GS)",
+        "hs" => "Hauptseminar  (HS)",
+        "re" => "Repetitorium  (RE)",
+        "kk" => "Klausurenkurs  (KK)",
+        "klv" => "Klinische Visite  (KLV)",
+        "ko" => "Kolloquium  (KO)",
+        "ks" => "Kombiseminar  (KS)",
+        "ku" => "Kurs  (KU)",
+        "ms" => "Mittelseminar  (MS)",
+        "os" => "Oberseminar  (OS)",
+        "pr" => "Praktikum  (PR)",
+        "prs" => "Praxisseminar  (PRS)",
+        "pjs" => "Projektseminar  (PJS)",
+        "ps" => "Proseminar  (PS)",
+        "sl" => "Sonstige Lehrveranstaltung  (SL)",
+        "tut" => "Tutorium  (TUT)",
+        "v-ue" => "Vorlesung mit Übung  (V/UE)",
+        "ue" => "Übung  (UE)",
+        "vorl" => "Vorlesung  (VORL)",
+        "hvl" => "Hauptvorlesung  (HVL)",
+        "pf" => "Prüfung  (PF)",
+        "gsz" => "Gremiensitzung  (GSZ)"
+    );
+    public static $lecturetypen_short = array(
+        "awa" => "Anleitung zu wiss. Arbeiten",
+        "ag" => "Arbeitsgemeinschaft",
+        "ak" => "Aufbaukurs",
+        "ek" => "Einführungskurs",
+        "ex" => "Exkursion",
+        "gk" => "Grundkurs",
+        "sem" => "Seminar",
+        "ts" => "Theorieseminar",
+        "es" => "Examensseminar",
+        "mas" => "Masterseminar",
+        "as" => "Aufbauseminar",
+        "gs" => "Grundseminar",
+        "hs" => "Hauptseminar",
+        "re" => "Repetitorium",
+        "kk" => "Klausurenkurs",
+        "klv" => "Klinische Visite",
+        "ko" => "Kolloquium",
+        "ks" => "Kombiseminar",
+        "ku" => "Kurs",
+        "ms" => "Mittelseminar",
+        "os" => "Oberseminar",
+        "pr" => "Praktikum",
+        "prs" => "Praxisseminar",
+        "pjs" => "Projektseminar",
+        "ps" => "Proseminar",
+        "sl" => "Sonstige Lehrveranstaltung",
+        "tut" => "Tutorium",
+        "v-ue" => "Vorlesung mit Übung",
+        "ue" => "Übung",
+        "vorl" => "Vorlesung",
+        "hvl" => "Hauptvorlesung",
+        "pf" => "Prüfung",
+        "gsz" => "Gremiensitzung"
+    );
+    public static $leclanguages = array(
+        "D" => "Deutsch"
+    );
+    public static $pubtypes = array(
+        "artmono" => "Artikel im Sammelband",
+        "arttagu" => "Artikel im Tagungsband",
+        "artzeit" => "Artikel in Zeitschrift",
+        "techrep" => "Interner Bericht (Technischer Bericht, Forschungsbericht)",
+        "hschri" => "Hochschulschrift (Dissertation, Habilitationsschrift, Diplomarbeit etc.)",
+        "dissvg" => "Hochschulschrift (auch im Verlag erschienen)",
+        "monogr" => "Monographie",
+        "tagband" => "Tagungsband (nicht im Verlag erschienen)",
+        "schutzr" => "Schutzrecht"
+    );
+    public static $hstypes = array(
+        "diss" => "Dissertation",
+        "dipl" => "Diplomarbeit",
+        "mag" => "Magisterarbeit",
+        "stud" => "Studienarbeit",
+        "habil" => "Habilitationsschrift",
+        "masth" => "Masterarbeit",
+        "bacth" => "Bachelorarbeit",
+        "intber" => "Interner Bericht",
+        "diskus" => "Diskussionspapier",
+        "discus" => "Discussion paper",
+        "forber" => "Forschungsbericht",
+        "absber" => "Abschlussbericht",
+        "patschri" => "Patentschrift",
+        "offenleg" => "Offenlegungsschrift",
+        "patanmel" => "Patentanmeldung",
+        "gebrmust" => "Gebrauchsmuster"
+    );
 
-		public static $leclanguages = array(
-		    "D" => "Deutsch"
-		);
-
-		public static $pubtypes = array(
-			"artmono" => "Artikel im Sammelband",
-            "arttagu" =>  "Artikel im Tagungsband",
-            "artzeit" => "Artikel in Zeitschrift",
-            "techrep" => "Interner Bericht (Technischer Bericht, Forschungsbericht)",
-            "hschri" => "Hochschulschrift (Dissertation, Habilitationsschrift, Diplomarbeit etc.)",
-            "dissvg" => "Hochschulschrift (auch im Verlag erschienen)",
-            "monogr" => "Monographie",
-            "tagband" => "Tagungsband (nicht im Verlag erschienen)",
-            "schutzr" => "Schutzrecht"
-        );
-		
-		public static $hstypes = array(
-			"diss" => "Dissertation",
-            "dipl" => "Diplomarbeit",
-            "mag" => "Magisterarbeit",
-            "stud" => "Studienarbeit",
-            "habil" => "Habilitationsschrift",
-            "masth" => "Masterarbeit",
-            "bacth" => "Bachelorarbeit",
-            "intber" => "Interner Bericht",
-            "diskus" => "Diskussionspapier",
-            "discus" => "Discussion paper",
-            "forber" => "Forschungsbericht",
-            "absber" => "Abschlussbericht",
-            "patschri" => "Patentschrift",
-            "offenleg" => "Offenlegungsschrift",
-            "patanmel" => "Patentanmeldung",
-            "gebrmust" => "Gebrauchsmuster"
-		);
-		
-	}
+}
 
 ?>
