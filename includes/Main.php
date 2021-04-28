92bf2122f39bd787aff457fabc6b4b3ace7d0f44
Benjamin Klemencic <benjamin.klemencic@fau.de>
2021-04-28 15:03:20 +0200

    3.2.8 (reactivate widget)

--- includes/Main.php
+++ includes/Main.php
@@ -44,7 +44,7 @@ class Main {
         $shortcode->onLoaded();
 
         // Widget
-        // add_action( 'widgets_init', [$this, 'loadWidget'] );    
+        add_action( 'widgets_init', [$this, 'loadWidget'] );    
         add_theme_support( 'widgets-block-editor' );
         apply_filters('gutenberg_use_widgets_block_editor', get_theme_support( 'widgets-block-editor' ));
     }
