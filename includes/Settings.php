fdc649107985154683a3b8e5fc6e281514616e22
Benjamin Klemencic <benjamin.klemencic@fau.de>
2021-04-29 13:23:35 +0200

    3.2.12 (gets old options from _rrze_univis)

--- includes/Settings.php
+++ includes/Settings.php
@@ -83,14 +83,23 @@ class Settings
     {
         $this->pluginFile = $pluginFile;
         $this->settingsPrefix = dirname(plugin_basename($this->pluginFile)) . '-';
+
+        // einmalig alte Parameter holen
+        $oldOptions = get_option( '_rrze_univis' );
+        if (!empty($oldOptions)){
+            foreach($oldOptions as $k => $v){
+                $oldOptions['basic_' . $k] =  $v;
+            }
+            update_option('rrze-univis', $oldOptions);
+            update_option('_rrze_univis', NULL);
+        }
     }
 
     /**
      * Er wird ausgeführt, sobald die Klasse instanziiert wird.
      * @return void
      */
-    public function onLoaded()
-    {
+    public function onLoaded(){
         $this->setMenu();
         $this->setSections();
         $this->setFields();
