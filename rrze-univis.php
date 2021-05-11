1e1fa6cd616681c4c5c170b28deb7a65613d6f31
Benjamin Klemencic <benjamin.klemencic@fau.de>
2021-05-11 17:05:26 +0200

    3.2.43 (fetch data if transient is "no data found" + att sprache)

--- rrze-univis.php
+++ rrze-univis.php
@@ -4,7 +4,7 @@
  * Plugin Name:     RRZE UnivIS
  * Plugin URI:      https://github.com/RRZE-Webteam/rrze-univis
  * Description:     Einbindung von Daten aus UnivIS
- * Version:         3.2.42
+ * Version:         3.2.43
  * Author:          RRZE-Webteam
  * Author URI:      https://blogs.fau.de/webworking/
  * License:         GNU General Public License v3
